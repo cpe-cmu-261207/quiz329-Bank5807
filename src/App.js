b65b4dc0eb302d1ba4bde25177e9e3fc4105dec8
Bank5807 <47866427+Bank5807@users.noreply.github.com>
2021-02-09 14:16:06 +0700

    first comit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import { useState, useEffect } from "react";
 
 function App() {
   //add useState for all state variables
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      name: '',
+      gender: '',
+      age: '',
+    }
+  }
+
+  handleGenderChange = (event) => {
+    this.setState({
+      gender: target.event.value
+    })
+  }
+  handleAgeChange = (event) => {
+    this.setState({
+      age: target.event.value
+    })
+  }
   //load locationStorage
   useEffect(() => {
     const items = localStorage.getItem("items");
-    // ...
-  }, []);
-
+    }
+  , []);
   return (
     <div className="card" style={{ width: 400 }}>
       <div className="card-content">
@@ -21,12 +39,19 @@ function App() {
             type="text"
             placeholder="e.q John Smith"
             //update related state based on event
+            value={this.state.name}
+            onChange={this.handleNameChange}
           ></input>
         </div>
 
         <div className="field">
           <label className="label">Gender</label>
-          <select className="input" type="text" placeholder="Please select ..">
+          <select 
+            className="input" 
+              type="text" 
+                placeholder="Please select .."
+                value={this.state.gender}
+                onChange={this.handleGenderChange}>
             <option value="" disabled selected hidden>
               -- Select Gender --
             </option>
@@ -37,7 +62,13 @@ function App() {
 
         <div className="field">
           <label className="label">Age</label>
-          <input className="input" type="number" placeholder="e.q 30"></input>
+          <input 
+            className="input" 
+            type="number" 
+            placeholder="e.q 30" 
+            value={this.state.Age} 
+            onChange={this.handleUseState}>
+          </input>
         </div>
 
         <button className="button is-primary is-fullwidth">Submit</button>
@@ -48,7 +79,8 @@ function App() {
         <p className="is-4 title has-text-centered">Person List</p>
         {/* sample table */}
         <ItemTable name={"Bob"} gender={"Male"} age={"50"} />
-        <p>Your name and code here</p>
+        <ItemTable name={this.name.value} gender={this.gender.value} age={this.age.value} />
+        <p>Tanat Wipasakunden 620610787</p>
       </div>
     </div>
   );
